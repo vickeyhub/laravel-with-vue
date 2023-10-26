e8ffa45d186022865c72b9e46de008a0868f194c
vishal maurya <vishalweb9@gmail.com>
2023-10-27 03:17:35 +0530

    modify the login screen for desktop view and mobile view with .display function

--- resources/js/pages/login.vue
+++ resources/js/pages/login.vue
@@ -1,36 +1,39 @@
 <template>
     <v-app>
-        <div class="login-wrapper h-screen w-100">
-            <div class="login-background d-flex">
-                <div
-                    class="w-50 login-text-content d-flex flex-column justify-center align-center"
-                >
-                    <h1 class="w-75 text-white text-h1 font-weight-bold">
+        <div class="login-wrapper h-screen" :class="{'w-100': $vuetify.display.smAndDown, 'w-100': $vuetify.display.mdAndUp}">
+
+            <div class="login-background d-flex" :class="{'flex-column': $vuetify.display.smAndDown}">
+
+                <div class="login-text-content d-flex" :class="{'flex-column w-100 text-center': $vuetify.display.smAndDown,'w-100 flex-column justify-center align-center': $vuetify.display.mdAndUp}">
+
+                    <h1 :class="{'w-100 mt-10 font-weight-bold': $vuetify.display.smAndDown, 'w-75 text-h1 font-weight-bold': $vuetify.display.mdAndUp }" class="text-white">
                         Advanture start here
                     </h1>
-                    <p class="w-75 text-white font-weight-bold">
+
+                    <p :class="{'w-100 mt-3': $vuetify.display.smAndDown, 'w-75': $vuetify.display.mdAndUp }" class="text-white font-weight-bold">
                         Login to Join Our Community
                     </p>
+
                 </div>
-                <div
-                    class="login-form w-50 d-flex flex-column justify-center align-center"
-                >
-                    <div class="logn-logo">
-                        <h1 class="text-center">Logo</h1>
-                        <p class="text-center">Hello Welcome Back!</p>
+
+                <div class="w-100 login-form d-flex flex-column justify-center align-center" :class="{'mt-5': $vuetify.display.smAndDown}">
+                    <div class="login-logo">
+                        <h1 class="text-center">LOGO</h1>
+                        <p class="text-center" :class="{'text-white mt-2': $vuetify.display.smAndDown}">Hello Welcome Back!</p>
                     </div>
-                    <form class="mt-16" action="">
-                        <v-responsive class="mx-auto" min-width="350px" max-width="500">
+                    <v-form @submit.prevent class="mt-16" action="">
+                        <v-responsive class="mx-auto" :min-width="$vuetify.display.smAndDown? '300px': '350px'" max-width="500">
                             <v-text-field
                                 clearable
                                 label="Email"
                                 variant="solo"
+                                v-model="email"
                                 :rules="[rules.email_required]"
                                 required
                             ></v-text-field>
                         </v-responsive>
 
-                        <v-responsive class="mx-auto" min-width="350px" max-width="500">
+                        <v-responsive class="mx-auto" :min-width="$vuetify.display.smAndDown? '300px': '350px'" max-width="500">
                             <v-text-field
                                 type="password"
                                 clearable
@@ -47,8 +50,8 @@
                             </div>
                             <div><router-link class="link" to="">Forget password</router-link></div>
                         </div>
-                        <v-btn class="bg-indigo w-100">Login</v-btn>
-                    </form>
+                        <v-btn type="submit" class="bg-indigo w-100">Login</v-btn>
+                    </v-form>
                 </div>
             </div>
         </div>
@@ -75,6 +78,7 @@ export default {
             password_required: (value) => !!value || "Password Field is required",
             min_length: (value) => (value.length >= 8 || `This field must be at least 8 characters`)
         },
+        email:'',
         password:'',
     }),
 };
